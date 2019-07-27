06c6c31c4e45bbb8ae0eb706dea87b68f5a494b6
comntr <>
2019-07-26 22:16:21 -0700

    /captcha

--- bin/captcha/index.js
+++ bin/captcha/index.js
@@ -3,27 +3,39 @@ define(["require", "exports", "src/log"], function (require, exports, logger) {
     Object.defineProperty(exports, "__esModule", { value: true });
     const $ = (sel) => document.querySelector(sel);
     const log = logger.tagged('captcha');
+    const HOST = 'http://127.0.0.1:2556';
     const ID_QUESTION = '#question';
     const ID_ANSWER = '#answer';
     const ID_SIGNATURE = '#signature';
     const ID_VERIFY = '#verify';
+    const ID_KEYS = '#keys';
     function init() {
         log.i('init()');
-        let hash = location.hash.slice(1) || 'abc';
+        let hash = getHash();
         let img = $(ID_QUESTION);
-        img.src += hash;
+        img.src = HOST + '/question/' + hash;
         $(ID_VERIFY).addEventListener('click', verify);
     }
     exports.init = init;
+    function getHash() {
+        return location.hash.slice(1) || 'abc';
+    }
     async function verify() {
         let answer = $(ID_ANSWER).textContent;
         log.i('answer:', answer);
-        let img = $(ID_QUESTION);
-        let url = img.src.replace(/\/question\/(.+)$/, '/postmark/$1?answer=' + answer);
+        let hash = getHash();
+        let url = HOST + '/postmark/' + hash + '?answer=' + answer;
         let res = await fetch(url);
         let text = await res.text();
         $(ID_SIGNATURE).textContent = res.status == 200 ?
             text : res.status + ' ' + res.statusText + ' ' + text;
+        await loadKeys();
+    }
+    async function loadKeys() {
+        let url = HOST + '/keys';
+        let res = await fetch(url);
+        let text = await res.text();
+        $(ID_KEYS).textContent = text;
     }
 });
 //# sourceMappingURL=index.js.map
